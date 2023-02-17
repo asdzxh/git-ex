e50b3a753efabd289ac03e5c3f6ad75634f68e21
asdzxh <2832009432@qq.com>
2023-02-17 08:59:29 +0800

    Java Web多模块学习:Servlet初体验

---
+++ servlet-learning/src/main/java/top/zxh/web/Main.java
@@ -0,0 +1,4 @@
+package top.zxh.web;
+
+public class Main {
+}
--- src/main/java/top/zxh/Main.java
+++
@@ -1,7 +0,0 @@
-package top.zxh;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
